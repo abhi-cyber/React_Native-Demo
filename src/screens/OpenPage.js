7423907dc21f6d431e23e0cb0d90ad7a4e8783c8
abhi-cyber <abhirajchatrath@gmail.com>
2022-05-08 11:57:15 +0530

    Opening Page edited

--- src/screens/OpenPage.js
+++ src/screens/OpenPage.js
@@ -1,21 +1,40 @@
-import { ImageBackground, StyleSheet, Text,SafeAreaView, View } from 'react-native';
+import { Image, StyleSheet, Text,SafeAreaView, View } from 'react-native';
 
-const image = { uri: "https://cdn.dribbble.com/users/3912043/screenshots/14503962/media/6506cd7891d9610a374aaabdb34976ff.jpg?compress=1&resize=1200x900&vertical=top" };
 
 export default function Open() {
   return (
     <SafeAreaView style={styles.container}>
-      <ImageBackground source={image} resizeMode="cover" style={styles.image} />
+      <Text style={styles.line1}>Welcome to,</Text>
+      <Text style={styles.line2}>PG Finder</Text>
+      <Text style={styles.line3}>We have best in{"\n"}class{"\n"}Recommindations{"\n"}for PG's.</Text>
+      <Image
+        source={require('..../assets/arrow-right.png')}
+      />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  },
-  image: {
     flex: 1,
-    justifyContent: "center"
+    backgroundColor: '#0E1114' 
+  },
+  line1: {
+    color: 'white',
+    fontWeight: "bold",
+    marginTop : 90,
+    fontSize: 20,
+  },
+  line2: {
+    color: 'white',
+    marginTop : 10,
+    fontWeight: "bold",
+    fontSize: 35
+  },
+  line3: {
+    color: '#696969',
+    marginTop : 30,
+    fontWeight: "bold",
+    fontSize: 35
   },
 });
